f39a9d6f774e4807deefd21438dda9d4002a36a7
karen-rhz <127526793+karen-rhz@users.noreply.github.com>
2023-04-26 21:33:55 -0400

    Initial display setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+import pandas
+from turtle import Turtle, Screen
+
+# Screen setup
+screen = Screen()
+screen.title("Discover Madagascar 🇲🇬")
+screen.bgpic("mdg_22-regions.gif")
+
+score = 0
+user_guess_list = []
+
+
+
